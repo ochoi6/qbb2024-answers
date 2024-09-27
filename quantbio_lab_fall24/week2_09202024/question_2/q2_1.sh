3df2b210f6d2d28f82426bafb91ff90e151bc81c
Olivia Choi <ochoi5@jh.edu>
2024-09-27 07:35:17 -0400

    Final version for question 2.1 wth comments

--- quantbio_lab_fall24/week2_09202024/question_2/q2_1.sh
+++ quantbio_lab_fall24/week2_09202024/question_2/q2_1.sh
@@ -22,40 +22,54 @@
     # Total 5 SNP files with different MAF levels and 4 feature files = 20 total combinations
  
 
-#Saving the results in a file called 'snp_counts.txt'
+# Creating a header in the new file containing output called 'snp_counts.txt' with 3 columns "MAF", "Feature", and "Enrichment" that are separated by tabs
 echo -e "MAF\tFeature\tEnrichment" > snp_counts.txt
 
-
+# Creating respective arrays to store the list of MAF (there should be a total of 4 files) and feature (there should be a total of 4) files 
 maf_files=("chr1_snps_0.1.bed" "chr1_snps_0.2.bed" "chr1_snps_0.3.bed" "chr1_snps_0.4.bed" "chr1_snps_0.5.bed")
 feature_files=("cCREs_chr1.bed" "exons_chr1.bed" "introns_chr1.bed" "other_chr1.bed")
 
+# Creating a for loop that runs through each individual maf file in the array of maf files 
 for maf in 0.1 0.2 0.3 0.4 0.5;  
 do 
+
+    # Creating a file name for the maf file being run through with the synonymous parts of name written out and creating a parameter to substitute each new file respectively 
     maf_file_name=chr1_snps_${maf}.bed
-    # Calculate coverage and SNP density for the genome
+
+    # Calculating bedtools coverage for the genome of chromosome 1 using bedtools and inputting the output of that into a new temp file for each maf file
     bedtools coverage -a genome_chr1.bed -b ${maf_file_name} > temp_file_${maf}.txt
+
+    # Summing the SNP counts using the 4th column from the output saved in the respective temp maf files and storing it in a variable called 'snp_sum_maf'
     snp_sum_maf=$(awk '{s+=$4}END{print s}' temp_file_${maf}.txt)
+
+    # Summing the base coverage counts using the 6th column from the output saved in the respective temp maf file and storing it in a variable called 'base_from_coverage_maf'
     base_from_coverage_maf=$(awk '{s+=$6}END{print s}' temp_file_${maf}.txt)
     
-    # Calculate genome-wide background SNP density
-    background_maf=$(echo "${snp_sum_maf} / ${base_from_coverage_maf}" | bc -l)
+    # Calculating genome-wide background SNP density using the equation + the necessary command options provided and storing it into a variable called 'background_maf'
+    background_maf=$(echo -e "${snp_sum_maf} / ${base_from_coverage_maf}" | bc -l)
 
-    # Loop through each feature file
+    # Looping through each feature file in the array of feature files using a nested for loop 
     for feature_file in cCREs exons introns other;
     do 
+        # Creating a file name for the feature file being run through with the synonymous parts of name written out and creating a parameter to substitute each new file respectively 
         feature_file_name=${feature_file}_chr1.bed
-        # Calculate coverage and SNP density for the feature
+
+        # Calculating bedtools coverage for the specific feature in chromosome 1 using bedtools and inputting the output of that into a feature new temp file for each feature file
         bedtools coverage -a ${feature_file_name} -b ${maf_file_name} > feature_temp_file${feature_file}.txt
+
+        # Summing the SNP counts using the 4th column from the output saved in the respective temp feature files and storing it in a variable called 'snp_sum_feature'
         snp_sum_feature=$(awk '{s+=$4}END{print s}' feature_temp_file${feature_file}.txt)
+
+        # Summing the base coverage counts using the 6th column from the output saved in the respective temp maf file and storing it in a variable called 'base_from_coverage_feature'
         base_from_coverage_feature=$(awk '{s+=$6}END{print s}' feature_temp_file${feature_file}.txt)
         
-        # Calculate feature SNP density
+        # Calculating the feature SNP density using the equation + necessary command options provided and storing it into a variable called 'feature_density'
         feature_density=$(echo "${snp_sum_feature} / ${base_from_coverage_feature}" | bc -l)
         
-        # Calculate enrichment (feature_density / background_maf)
-        enrichment=$(echo "${feature_density} / ${background_maf}" | bc -l)
+        # Calculating enrichment using this equation enrichment = feature_density / background_maf and storing it into a variable called 'enrichment'
+        enrichment=$(echo -e "${feature_density} / ${background_maf}" | bc -l)
 
-        # Append results to the output file
+        # Appending the results to the output file 'snp_counts.txt'
         echo -e "${maf}\t${feature_file}\t${enrichment}" >> snp_counts.txt
     done
 done
